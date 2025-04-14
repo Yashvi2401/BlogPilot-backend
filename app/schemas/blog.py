61ec4a2caa9ad000f24c7a717ef76648a01c38bd
yashvi <yashvipalkhiwala2401@gmail.com>
2025-04-14 18:18:50 +0530

    modify blogbase

--- app/schemas/blog.py
+++ app/schemas/blog.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pydantic import BaseModel
 from typing import Optional
 
@@ -17,6 +18,8 @@ class BlogUpdate(BaseModel):
 
 class BlogOut(BlogBase):
     id: int
-    author:str   
+    author:str
+    created_at: datetime  
+    updated_at: Optional[datetime] = None    
     class Config:
         from_attributes  = True
